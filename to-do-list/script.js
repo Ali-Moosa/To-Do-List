a9a0a579dbddc5b38bdbf618f6f12655c42fe27c
Ali-Moosa <newlevel2119@gmail.com>
2025-09-15 22:23:52 +0500

    Add files via upload

---
+++ to-do-list/script.js
@@ -0,0 +1,45 @@
+const AddBtn = document.querySelector("#add");
+let ule = document.querySelector("#task-ul");
+const task = document.querySelector("#texxt");
+let update = null;
+
+AddBtn.addEventListener("click", () => {
+  if (task.value.trim() === "") return;
+  if (update) {
+    update.textContent = task.value;
+    update = null;
+    AddBtn.textContent = "Add";
+    task.value = "";
+  } else {
+    ule.innerHTML += `<div class="task-div">
+    <div class="upper">
+            <input class="cheek" type="checkbox" />
+          <li class=>${task.value}</li>
+          </div>
+          <div class="btns">
+            <button class="del">Delete</button>
+            <button class="edit">Edit</button>
+          </div>
+        </div>`;
+  }
+  task.value = "";
+});
+
+ule.addEventListener("click", (e) => {
+  if (e.target.classList.contains("del")) {
+    e.target.closest(".task-div").remove();
+  }
+
+  if (e.target.classList.contains("edit")) {
+    const taskDiv = e.target.closest(".task-div");
+    const lis = taskDiv.querySelector("li");
+    task.value = lis.textContent;
+    update = lis;
+    AddBtn.textContent = "Update";
+  }
+  if (e.target.classList.contains("cheek")) {
+    const taskDiv = e.target.closest(".task-div");
+    const lis = taskDiv.querySelector("li");
+    lis.classList.toggle("completed");
+  }
+});
